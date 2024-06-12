e0572a57c4923b257f353aa1feecbd068c963237
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-06-12 15:37:15 +0700

    response api interface

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -3,3 +3,4 @@ import cartItemModel from "./cartItemModel";
 import shoppingCartModel from "./shoppingCartModel";
 import userModel from "./userModel";
 export type { menuItemModel, cartItemModel, shoppingCartModel, userModel };
+  
