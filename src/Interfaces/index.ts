f4039137344c8ba0fcbcc94e02ce0b895f2bfb81
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-06-12 15:26:50 +0700

    controller components in register

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -2,4 +2,4 @@ import menuItemModel from "./menuItemModel";
 import cartItemModel from "./cartItemModel";
 import shoppingCartModel from "./shoppingCartModel";
 import userModel from "./userModel";
-export type { menuItemModel, cartItemModel, shoppingCartModel,userModel };
+export type { menuItemModel, cartItemModel, shoppingCartModel, userModel };
