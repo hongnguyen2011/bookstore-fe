e8eb07ffa2ba3c5f6fe491872e49f56be7400664
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-06-13 16:05:10 +0700

    fix bug

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -13,13 +13,23 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useGetShoppingCartQuery } from "../Apis/shoppingCartApi";
 import { setShoppingCart } from "../Storage/Redux/shoppingCartSlice";
-
+import { jwtDecode } from "jwt-decode";
+import { userModel } from "../Interfaces";
+import { setLoggedInUser } from "../Storage/Redux/userAuthSlice";
 function App() {
   const dispatch = useDispatch();
 
   const { data, isLoading } = useGetShoppingCartQuery(
     "b7ae37bf-09b1-4b47-9ce1-c963031d2920"
   );
+
+  useEffect(() => {
+    const localToken = localStorage.getItem("token");
+    if (localToken) {
+      const { fullName, id, email, role }: userModel = jwtDecode(localToken);
+      dispatch(setLoggedInUser({ fullName, id, email, role }));
+    }
+  }, []);
 
   useEffect(() => {
     if (!isLoading) {
